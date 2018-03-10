f38d1aac23dd57d6253eccc6c6222df1b5ec3486
Ralf Northman <ralf.northman@bredband.net>
2018-03-10 17:38:10 +0100

    Bughunting array insert.

--- json_as_input_array.sql
+++ json_as_input_array.sql
@@ -6,7 +6,7 @@ use json_as_input_array;
 create table measure(
   measure_id serial primary key,
   object_id bigint unsigned not null,
-  postion_id bigint unsigned not null,
+  position_id bigint unsigned not null,
   efficiency_A decimal(5,3) unsigned,
   efficiency_B decimal(5,3) unsigned,
   is_latest bool not null default 1,
@@ -18,7 +18,7 @@ create table measure(
 );
 
 insert into measure 
-  (object_id, postion_id, efficiency_A, efficiency_B) values
+  (object_id, position_id, efficiency_A, efficiency_B) values
   (10, 34, 90.3, null);
 
 
@@ -26,7 +26,7 @@ insert into measure
 delimiter //
 create procedure insert_measure(
     in in_object_id bigint unsigned, 
-    in in_postion_id bigint unsigned,
+    in in_position_id bigint unsigned,
     in in_efficiency_A decimal(5,3) unsigned, 
     in in_efficiency_B decimal(5,3) unsigned 
   )
@@ -42,11 +42,11 @@ begin
 
   update measure set is_latest = 0
   where object_id = in_object_id and
-       postion_id = in_postion_id;
+       position_id = in_position_id;
 
   insert into measure set
     object_id  = in_object_id, 
-    postion_id = in_postion_id,
+    position_id = in_position_id,
     efficiency_A = in_efficiency_A,
     efficiency_B = in_efficiency_B;
 
@@ -54,6 +54,7 @@ begin
 
 end;//
 delimiter ;
+
 
 call insert_measure(10, 34, 77.3, null);
 call insert_measure(10, 34, 65.7, null);
@@ -64,18 +65,26 @@ call insert_measure(11, 87, null, 87.3);
 
 select efficiency_A, efficiency_B, is_latest, creation 
   from measure;
-select object_id, postion_id, efficiency_A, efficiency_B, is_latest 
+select object_id, position_id, efficiency_A, efficiency_B, is_latest 
   from measure;
 
-/*
+
+
 delimiter //
-create procedure insert_measure(
+create procedure array_insert(
     in in_object_id bigint unsigned, 
-    in in_postion_id bigint unsigned not null,
+    in in_position_id bigint unsigned,
     in in_effA_array json, 
     in in_effB_array json 
   )
 begin
+
+  declare effA_left json default in_effA_array;
+  declare effB_left json default in_effB_array;
+  declare effA, effB decimal(5,3) unsigned;
+  declare pos_id bigint unsigned default in_position_id;
+  declare array_length smallint unsigned default json_length(in_effA_array);
+  declare _first char(4) default '$[0]';
   
   declare exit handler for sqlexception
   begin
@@ -83,20 +92,39 @@ begin
     resignal;
   end;
 
+  if json_length(in_effA_array) <> json_length(in_effB_array) then
+    signal sqlstate '45000' set
+    mysql_errno = 45000,
+    message_text = 'Arrays have mismatched lengths.'
+  end if;
+
+
   start transaction;
 
-  update measure set is_latest = 0
-  where object_id = in_object_id and
-       postion_id = in_postion_id;
+  while @array_length > 0 do
 
-  insert into measure set
-    object_id  = in_object_id, 
-    postion_id = in_postion_id,
-    effA_array = in_effA_array,
-    effB_array = in_effB_array;
+    set @effA = json_extract(@effA_left, '$[0]'),
+        @effB = json_extract(@effB_left, '$[0]'),
+        @effA_left = json_remove(@effA_left, '$[0]'),
+        @effB_left = json_remove(@effB_left, '$[0]');
+
+    call insert_measure(in_object_id, @pos_id, @effA, @effB);
+    
+    set @pos_id = @pos_id + 1,
+        @array_length = @array_length - 1;
+
+  end while; 
 
   commit;
 
 end;//
 delimiter ;
-*/
+
+
+call array_insert(60, 101, '[77.3, 65.7, 74.2]', '[null, null, null]');
+call array_insert(60, 103, '[47.3, 45.7, 44.2]', '[null, null, null]');
+
+select efficiency_A, efficiency_B, is_latest, creation 
+  from measure;
+select object_id, position_id, efficiency_A, efficiency_B, is_latest 
+  from measure;
